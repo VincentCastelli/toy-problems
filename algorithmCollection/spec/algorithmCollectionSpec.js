366d928cf6d8feb2800735d23353b0fc4c32c1c0
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-11-18 22:37:56 -0800

    2 solutions for 2:isPalindrome

--- algorithmCollection/spec/algorithmCollectionSpec.js
+++ algorithmCollection/spec/algorithmCollectionSpec.js
@@ -5,3 +5,19 @@ describe("String Reversal", () => {
    assert.equal(reverse("Hello World!"), "!dlroW olleH");
   });
  });
+
+ describe("Palindrome", () => {
+  it("Should return true", () => {
+   assert.equal(isPalindrome("Cigar? Toss it in a can. It is so tragic"), true);
+  });
+  it("Should return false", () => {
+   assert.equal(isPalindrome("sit ad est love"), false);
+  });
+ });
+
+ describe("Integer Reversal", () => {
+  it("Should reverse integer", () => {
+   assert.equal(reverse(1234), 4321);
+   assert.equal(reverse(-1200), -21);
+  });
+ });
--- algorithmCollection/src/algorithmCollection.js
+++ algorithmCollection/src/algorithmCollection.js
@@ -15,7 +15,25 @@ const _reverse = (string) => {
   return result;
 }
 //HOF reducer approach
-const reverse = string => string.split("").reduce((result, character) => character + result);
+const __reverse = string => string.split("").reduce((result, character) => character + result);
 
 
-// <--- 2:  --->
+// <--- 2: Palindrome  --->
+
+// HOF reduce with equality approach 
+const isPalindrome = string => {
+  const validChars = "abcdefghijklmnopqrstuvwxyz".split("");
+  const stringChars = string.toLowerCase().split("").reduce((chars, char) =>
+              validChars.indexOf(char) > -1 ? chars.concat(char) : chars, []);
+  return stringChars.join("") === stringChars.reverse().join("");
+};
+
+// Native every approach 
+// const isPalindrome = (string) => {
+//   string.toLowerCase().split("").every((char, idx) => {
+//     char === string[string.length - 1 - idx];
+//   });
+// }
+
+
+// <--- 3: Integer Reversal  --->
